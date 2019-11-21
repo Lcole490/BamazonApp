40067e095b0d75734e9b7e7d4d50232180df3b3e
Lcole.engr <lcole.engr@gmail.com>
2019-11-20 20:15:59 -0500

    manager file commented for future understanding

--- bamazonManager.js
+++ bamazonManager.js
@@ -35,10 +35,11 @@ connection.connect(); // This line established connection to the created connect
 
 
 
-
+//****************************** F U N C T I O N _ _ F O R _ _ M A I N _ _ M E N U *****************************************
 
 // function which prompts the user for what action they should take
 function start() {
+    // Next 7 lines of console logs are strictly serving as banner for menu page
     console.log("");
     console.log("*****************************************************************");
     console.log("B A M A Z O N __  M A N A G E R __ V I E W");
@@ -47,36 +48,42 @@ function start() {
     console.log("              B-M-V MAIN PAGE");
     console.log("");
 
-    inquirer
+    
+    inquirer            // Inquirer package used to enable user interaction and input in CLI
         .prompt({
-            name: "managerOptions",
-            type: "list",
-            message: " What action would you like to perform?\n",
-            choices: ["View Products for Sale",
+            name: "managerOptions",     // name to reference from response in .then portion of prompt
+            type: "list",           // list type only allows user to chose from preset options set up below in choices portion of prompt
+            message: " What action would you like to perform?\n",   // question that user will be asked
+            choices: ["View Products for Sale",             // choices allows set list from which user can choose on screen
                 "View Low Inventory",
                 "Add to Inventory",
                 "Add New Product",
                 "EXIT"]
         })
-        .then(function (answer) {
-            // based on their answer, either call the bid or the post functions
-            if (answer.managerOptions === "View Products for Sale") {
-                productSales();
+        .then(function (answer) {      
+            // based on their answer from above, a different function is called to fulfill request
+            if (answer.managerOptions === "View Products for Sale") {       // if user chose the view products for sale option...
+                productSales();             // function lets user view products for sale
             }
             else if (answer.managerOptions === "View Low Inventory") {
-                lowInventory();
+                lowInventory();             // function lets user view items with low inventory
             }
             else if (answer.managerOptions === "Add to Inventory") {
-                addInventory();
+                addInventory();             // function lets user add inventory to item of their choosing
             }
             else if (answer.managerOptions === "Add New Product") {
-                addProduct();
+                addProduct();               // function lets user add products to database
             } else {
-                connection.end();
+                connection.end();           // connection is ended here if user chooses exit option
             }
         });
 }
 
+// *********************************E N D _ _ O F _ _ M A I N _ _ M E N U _ _ F U N C T I O N ***********************************************
+
+
+
+// ************************** F U N C T I O N _ _ F O R _ _ V I E W I N G _ _ A L L _ _ P R O D U C T S *************************************
 
 var productSales = function () {
     connection.query("SELECT * FROM products", function (err, res) {      // queries database to show all columns and rows in products table
@@ -84,9 +91,9 @@ var productSales = function () {
 
         // Below are the settings for the table, this format is taken from documentation from cli-table2 package
         var table = new Table({
-            head: ['ITEM ID', 'PRODUCT NAME', 'PRICE', 'QUANTITY']
-            , colWidths: [20, 20, 10, 20],
-            colAligns: ["center", "left", "right", "right"],
+            head: ['ITEM ID', 'PRODUCT NAME', 'PRICE', 'QUANTITY']          // Column names established
+            , colWidths: [20, 20, 10, 20],                              // Column widths established
+            colAligns: ["center", "left", "right", "right"],            // Alignment of items within columns
             style: {
                 head: ["aqua"],
                 compact: true
@@ -105,10 +112,16 @@ var productSales = function () {
 
     });
     console.log("\n\n");
-    start();
+    start();                // Allows program to cycle back to main menu in case user wants to perform different action
 };
 
 
+// ***************************************** E N D _ _ O F _ _ F U N C T I O N ***************************************************************
+
+
+
+
+// ************************** F U N C T I O N _ _ F O R _ _ V I E W I N G _ _ L O W _ _ I N V E N T O R Y *************************************
 
 
 var lowInventory = function () {
@@ -117,9 +130,9 @@ var lowInventory = function () {
 
         // Below are the settings for the table, this format is taken from documentation from cli-table2 package
         var table = new Table({
-            head: ['ITEM ID', 'PRODUCT NAME', 'PRICE', 'QUANTITY']
-            , colWidths: [20, 20, 10, 20],
-            colAligns: ["center", "left", "right", "right"],
+            head: ['ITEM ID', 'PRODUCT NAME', 'PRICE', 'QUANTITY']          // Column Names established
+            , colWidths: [20, 20, 10, 20],                              // Column widths established
+            colAligns: ["center", "left", "right", "right"],        // Establish alignment of items within columns
             style: {
                 head: ["aqua"],
                 compact: true
@@ -142,74 +155,73 @@ var lowInventory = function () {
     start();
 };
 
+// ***************************************** E N D _ _ O F _ _ F U N C T I O N ********************************************************
 
 
-// function to handle posting new items up for auction
+
+// ******************************** F U N C T I O N _ _ F O R _ _ A D D I N G _ _ I N V E N T O R Y *************************************
+
+
 function addInventory() {
-    // prompt for info about the item being put up for auction
-    inquirer
+    
+    inquirer        // Inquirer package enables user interaction / input in CLI
         .prompt([
             {
-                name: "item",
-                type: "input",
-                message: "What is the item would you like to replenish? (use item id)"
+                name: "item",          // name of prompt data container
+                type: "input",          // Input type takes in user typed data and saves it as ans.item
+                message: "What is the item would you like to replenish? (use item id)" // first question user is asked
             },
             {
-                name: "currentyQuantity",
-                type: "input",
-                message: "How many units are currently in stock?"
+                name: "currentQuantity",           // name of prompt data container
+                type: "input",              // input type takes user typed data and saves it as ans.currentQuantity
+                message: "How many units are currently in stock?" // Second question user is asked
             },
             {
-                name: "addedQuantity",
-                type: "input",
-                message: "How many units are you adding to the stock?",
-                validate: function (value) {
+                name: "addedQuantity",          // name of prompt data container
+                type: "input",              // input type takes user typde data and saves it as ans.addedQuantity
+                message: "How many units are you adding to the stock?",     // Third question user is asked
+                validate: function (value) {            // validation to ensure entered information is actually a number
                     if (isNaN(value) === false) {
                         return true;
                     }
-                    return false;
+                    return false;               // If input is not a number, then user function is cycled to beginning
                     console.log("Not a valid entry...Try again")
-                    addInventory();
+                    addInventory();         // cycles user back to the beginning of function by calling again
                 }
             }
         ])
-        .then(function (answer) {
-            // when finished prompting, insert a new item into the db with that info
-            // connection.query(
-            //     "SELECT stock_quantity FROM products WHERE item_id =answer.item", function (err, res) {      // queries database to show all columns and rows in products table
-            //         if (err) throw err;                                            // error handling
-            //         var currentQuantity = res;
-            //     });
+        .then(function (answer) {   // function runs after prompts are completed
 
-            var addedStock = parseInt(answer.currentyQuantity) + parseInt(answer.addedQuantity);
-            connection.query(
-                "UPDATE products SET ? WHERE ?",
+                        // Added stock is the number returned from the sum of current stock to amount of stock added by user
+            var addedStock = parseInt(answer.currentQuantity) + parseInt(answer.addedQuantity);
+            connection.query(                   // query database to target specific information
+                "UPDATE products SET ? WHERE ?",                // Update the stock quantity of item id entered by user with added stock
                 [
                     {
-                        stock_quantity: addedStock,
+                        stock_quantity: addedStock,             // Takes place of first "?" above
                     },
                     {
-                        item_id: answer.item,
+                        item_id: answer.item,               // Takes place of second "?" above
 
                     }
                 ],
-                function (err) {
-                    if (err) throw err;
+                function (err) {            // Error Handling
+                    if (err) throw err;             // Throws appropriate error
                     console.log("\nThere are now " + addedStock + " units of product with Item ID: " + answer.item + "\n");
                     console.log("Inventory has been updated successfully! \n");
-                    // re-prompt the user for if they want to bid or post
-                    start();
+                    
+                    start();            // Allows user to head to main menu for further tasks without stopping connection
                 }
             );
         });
 }
 
 
+// ***************************************** E N D _ _ O F _ _ F U N C T I O N ********************************************************
 
 
 
 
+// ******************************************* M A I N _ _ C O D E **********************************************************************
 
-
-
-start();
+start();                // Calls main menu function, which is all app needs to function
